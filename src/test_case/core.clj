fb48ed8147b6ebdbf8458807079e2fb23b700eba
Christos Av <xristosabra@gmail.com>
2024-01-28 19:50:53 +0200

    New function
    Sample of a function providing the average of a sum of integers

--- src/test_case/core.clj
+++ src/test_case/core.clj
@@ -1,7 +1,13 @@
 (ns test-case.core
   (:gen-class))
 
+(defn average
+  [numbers]
+  (prn (/ (apply + numbers) (count numbers))))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
