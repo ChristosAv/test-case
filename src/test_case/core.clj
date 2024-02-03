9cc984fadacfbc384e2ff0749db6765b4c0cfe80
Christos Av <xristosabra@gmail.com>
2024-02-03 23:38:28 +0200

    Simple lists of basic operations
    Added basic Math functions and a def var.

--- src/test_case/core.clj
+++ src/test_case/core.clj
@@ -1,13 +1,21 @@
 (ns test-case.core
   (:gen-class))
 
+(def v [42 "foo" 99.2 [5 12]])
+
 (defn average
   [numbers]
-  (prn (/ (apply + numbers) (count numbers))))
+  (/ (apply + numbers) (count numbers)))
 
+(defn list-quotation [& x]
+  (prn '(x)))
+
+(defn hypot [x y]
+  (let [x-2 (* x x)
+        y-2 (* y y)]
+    (Math/sqrt (x-2 y-2))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
-
