6145841712cce273195c1312d1df3fc2741a2489
Christos Av <xristosabra@gmail.com>
2024-02-05 22:26:32 +0200

    More functions
    Added more sample lists of the same chapter.
    Def vars are also included for REPL pracice.

--- src/test_case/core.clj
+++ src/test_case/core.clj
@@ -1,14 +1,36 @@
 (ns test-case.core
+  (:import java.util.Date)
   (:gen-class))
 
 (def v [42 "foo" 99.2 [5 12]])
+
+(def dest-map {:a 5 :b 6
+               :c [7 8 9]
+               :d {:e 10 :f 11}
+               "foo" 88
+               42 false})
+
+(defn make-user
+  [username & {:keys [email join-date]
+               :or {join-date (java.util.Date.)}}]
+  {:username username
+   :join-date join-date
+   :email email
+   ;; 2.592e9 -> one month in ms
+   :exp-date (java.util.Date. (long (+ 2.592e9 (.getTime join-date))))})
+
+;;Variadic function
+(def strange-adder (fn
+                     ([x] (+ x 1))
+                     ([x y] (+ x y))))
 
 (defn average
   [numbers]
   (/ (apply + numbers) (count numbers)))
 
-(defn list-quotation [& x]
-  (prn '(x)))
+(defn list-quotation [x]
+  (let [local-x 5]
+      (local-x)))
 
 (defn hypot [x y]
   (let [x-2 (* x x)
