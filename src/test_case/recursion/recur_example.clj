64ed6d591a9949d08e31826ac82845fed8bb024a
Christos Av <xristosabra@gmail.com>
2024-02-17 18:14:34 +0200

    2 variations of recursion

--- src/test_case/recursion/recur_example.clj
+++ src/test_case/recursion/recur_example.clj
@@ -1,1 +1,18 @@
 (ns test-case.recursion.recur-example)
+
+(defn count-down
+  []
+  (loop [x 5]
+    (if (zero? x)
+      :blastoff!
+      (do (println x)
+          (recur (dec x))))))
+
+(defn actual-count-down
+  []
+  (loop [x 5]
+    (if (zero? x)
+      :blastoff!
+      (do (println x)
+          (Thread/sleep 1000)
+          (recur (dec x))))))
